5de8ae5aafd39fc984689149302c2e757d583cf0
irnscvngr <patrickhausmann@gmx.de>
2025-02-17 13:26:22 +0100

    added tests.yml, adjusted deploy.yml, added linting, added unittests, cleaned some code

--- cloud_functions/hello_world/main.py
+++ cloud_functions/hello_world/main.py
@@ -1,5 +1,11 @@
+"""
+Super basic test function to get started on GCP.
+"""
 import functions_framework
 
 @functions_framework.http
-def hello_world(request):  # This is your Cloud Function
+def hello_world():
+    """
+    Just return a message.
+    """
     return "Hello World! This is a test-run pushed from GitHub.", 200
