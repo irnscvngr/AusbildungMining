ff5f49d82194ca1d88901164bff0d42a53999263
irnscvngr <patrickhausmann@gmx.de>
2025-02-19 11:11:38 +0100

    first setup of request-args switching

--- cloud_functions/toggle_cloudsql/main.py
+++ cloud_functions/toggle_cloudsql/main.py
@@ -4,7 +4,7 @@ Main function that acts as entry point for Google Cloud Run Functions
 # pylint:disable=import-error
 import functions_framework
 
-from start_cloudsql import start_cloud_sql
+from toggle_cloudsql import toggle_cloudsql
 
 # pylint:disable=unused-argument
 @functions_framework.http
@@ -12,8 +12,9 @@ def main(request):
     """
     Entry point for GCP.
     """
+    request_type = request.args.get('type')  # Get the 'type' parameter
     # maybe use start and stop in request - then adjust cron-jobs as needed!
-    start_cloud_sql()
+    # toggle_cloudsql()
 
     # Return results as string
-    return 'Done.', 200
+    return request_type, 200
