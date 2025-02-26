a46689afb69eb6b2d8b6b0d6e542a2558aff8c11
irnscvngr <patrickhausmann@gmx.de>
2025-02-26 14:52:14 +0100

    Adjusted official_stats main function so it tests a private and pbulic connection and then attemps a POST-request to the DB-endpoint. tested on GCP!

--- cloud_functions/official_stats/main.py
+++ cloud_functions/official_stats/main.py
@@ -2,9 +2,32 @@
 Main function that acts as entry point for Google Cloud Run Functions
 """
 # pylint:disable=import-error
-import functions_framework
+import os
+import warnings
 
-from official_stats import get_official_stats, write_to_sql
+import functions_framework
+import requests
+
+from google.cloud import secretmanager
+# For authenticating with database-endpoint cloud function
+import google.auth.transport.requests
+import google.oauth2.id_token
+
+from official_stats import get_official_stats
+
+def get_secret(secret_name):
+    """
+    Retrieves a secret from Secret Manager
+    to setup database connection later.
+    """
+    # Get project ID from environment variables
+    project_id = os.environ.get('GCP_PROJECT_ID')
+    # Setup connection to GCP secret manager
+    name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    client = secretmanager.SecretManagerServiceClient()
+    response = client.access_secret_version(request={"name": name})
+    # Return secret value
+    return response.payload.data.decode("UTF-8")
 
 # pylint:disable=unused-argument
 @functions_framework.http
@@ -12,16 +35,53 @@ def main(request):
     """
     Entry point for GCP.
     """
-    # Get official values as dictionary
-    off_stats_dict = get_official_stats()
+    # Get the cloud function's URL to setup a request
+    db_endpoint_url = get_secret('DB_ENDPOINT_RUN_URL')
 
-    # Write results to database
-    write_to_sql(off_stats_dict)
+    audience = get_secret('DB_ENDPOINT_URL')
+    
+    # Some authentication stuff...
+    auth_req = google.auth.transport.requests.Request()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
+    
+    # Setup header to send ID token for authentication
+    headers = {"Authorization": f"Bearer {id_token}"}
 
-    # Initialize empty string and add all results to it
-    res_string = ""
-    for key,value in off_stats_dict.items():
-        res_string += f"{key} : {value}\n"
+    # Test call to database-endpoint to verify internal/PRIVATE connection
+    try:
+        response = requests.get(db_endpoint_url,
+        headers=headers,
+        timeout=20)
+        print('--Private request successful!--')
+        print(f"Empty GET request to database endpoint: {response.status_code}, {response.content}")
+    except Exception as e:
+        warnings.warn(f'Private request failed! {e}')
 
-    # Return results as string
-    return res_string, 200
+    # Test call to curlmyip to verify external/PUBLIC connection
+    try:
+        response = requests.get("https://curlmyip.org/",timeout=20)
+        print('--Public request successful!--')
+        print(f"External call: {response.status_code}, {response.content}")
+    except Exception as e:
+        warnings.warn(f'Public request failed! {e}')
+
+    # Scraping and posting to database
+    try:
+        # Get official values from Ausbildung.de as dictionary
+        off_stats_dict = get_official_stats()
+        # Add database  information
+        off_stats_dict['schema_name'] = 'AusbildungMining'
+        off_stats_dict['table_name'] = 'official_stats'
+
+        # Send request to database endpoint using ID token for authentication
+        response = requests.post(db_endpoint_url,
+        headers=headers,
+        params=off_stats_dict,
+        timeout=20)
+
+        # Return results as string
+        return response.content, response.status_code
+    except Exception as e:
+        # Raise warning and return it
+        warnings.warn(f"Retrieval of official stats failed. {e}")
+        return e, 500
