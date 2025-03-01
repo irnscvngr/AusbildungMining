9c47eb6c197adfedc2e42fdd2d3758e3ca1437e0
irnscvngr <patrickhausmann@gmx.de>
2025-03-01 20:05:07 +0100

    Added choropleth map and some various features to the streamlit app

--- frontend_streamlit/main.py
+++ frontend_streamlit/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-from streamlit_backend import get_vacancies
+import streamlit_backend as stb
 
 st.set_page_config(page_title="AusbildungMining")
 
@@ -16,31 +16,83 @@ Interactive data visualization web app.
 """
 
 
-vacancies = get_vacancies()
+tab1, tab2, tab3 = st.tabs(["Arbeit", "Ausbildung", "Sonstiges"])
 
-vacancies_professions = (vacancies
-                         .groupby(['timestamp','Beruf'],as_index=False)
-                         .agg({'Beruf':'first','Plätze':'sum'})
-                         .set_index('timestamp'))
+with tab1:
+    st.write('Under construction...')
 
-professions = pd.Series(vacancies_professions['Beruf'].unique())
+with tab2:
 
-professions_select = st.multiselect(label='Berufe auswählen',
-                                    default=professions.sample(10),
-                                    key='profselector',
-                                    max_selections = 10,
-                                    options=professions)
+    vacancies = stb.get_vacancies()
 
-vacancies_select = vacancies_professions[vacancies_professions['Beruf'].isin(professions_select)]
+    professions = pd.Series(vacancies['Beruf'].unique()).sort_values()
 
-fig = px.line(vacancies_select,
-              color='Beruf')
-st.plotly_chart(fig)
+    timestamps = pd.Series(vacancies['timestamp'].unique())
+
+    # PROFESSION SELECT
+    if 'prof_select' not in st.session_state:
+        st.session_state['prof_select'] = professions.sample(1).values[0]
+
+    shuffle = st.button(label='Zufälliger Beruf',
+                        key='prof_shuffle')
+
+    if shuffle:
+        st.session_state['prof_select'] = professions.sample(1).values[0]
+
+    prof_select = st.selectbox(label='Ausbildungsberuf auswählen',
+                            options=professions,
+                            key='prof_select')
 
 
-official_stats_df = pd.read_csv('frontend_streamlit/data/AusbildungMining/official_stats_2025-02-26.csv')
+    vacancies = vacancies[vacancies['Beruf']==prof_select]
 
-official_stats_df['timestamp'] = pd.to_datetime(official_stats_df['timestamp'])
+    vacancies = stb.add_geoinfo_to_vacancies(vacancies)
 
-fig = px.line(official_stats_df.set_index('timestamp'))
-st.plotly_chart(fig)
+    # PLOT VALUES OVER TIME
+    df_temp = (vacancies
+            .groupby(['Bundesland','timestamp'],as_index=False)
+            .agg({'Plätze':'sum','Bundesland':'first'})
+            .set_index('timestamp'))
+
+    fig = px.line(df_temp,
+                y='Plätze',
+                color='Bundesland')
+
+    fig.update_layout(margin={"r":0,"t":60,"l":0,"b":0},
+                    xaxis_title='',
+                    height=500)
+
+    st.plotly_chart(fig)
+
+    # TIMESTAMP SELECT
+    if 'date_select' not in st.session_state:
+        st.session_state['date_select'] = timestamps.values[-1]
+
+    date_select = st.select_slider(label='Datum auswählen',
+                                options=timestamps,
+                                key='date_select')
+
+    vacancies = vacancies[vacancies['timestamp']==date_select]
+
+    vacancies = (vacancies
+                .groupby('Bundesland',as_index=False)
+                .agg({'Plätze':'sum',
+                    'Bundesland':'first',
+                    'id':'first'}))
+
+
+    fig = stb.plot_map(vacancies)
+
+    st.plotly_chart(fig)
+
+
+    official_stats_df = stb.get_official_stats()
+
+    official_stats_df['timestamp'] = pd.to_datetime(official_stats_df['timestamp'])
+
+    fig = px.line(official_stats_df.set_index('timestamp'))
+    st.plotly_chart(fig)
+
+
+with tab3:
+    st.write('Under construction...')
--- frontend_streamlit/streamlit_backend.py
+++ frontend_streamlit/streamlit_backend.py
@@ -1,11 +1,74 @@
 """
 Backend operations for the streamlit app
 """
-
+import json
+from urllib.request import urlopen
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 @st.cache_data
 def get_vacancies():
     vacancies = pd.read_parquet('frontend_streamlit/data/AusbildungMining/vacancies_2025-02-26.parquet')
     return vacancies
+
+@st.cache_data
+def get_official_stats():
+    official_stats_df = pd.read_csv('frontend_streamlit/data/AusbildungMining/official_stats_2025-02-26.csv')
+    return official_stats_df
+
+# Landkreis-geoJSON:
+with open("frontend_streamlit/data/maps/landkreise_simplify200.geojson",'r',encoding='utf-8') as response:
+    counties = json.load(response)
+
+# Bundesland-geoJSON
+with urlopen('https://github.com/isellsoap/deutschlandGeoJSON/raw/refs/heads/main/2_bundeslaender/3_mittel.geo.json') as response:
+    states = json.load(response)
+
+with open("frontend_streamlit/data/maps/bundesland_id-to-state.json",'r',encoding='utf-8') as response:
+    id_to_state = json.load(response)
+
+state_to_id = {}
+for key,value in id_to_state.items():
+    state_to_id[value] = key
+
+with open("frontend_streamlit/data/maps/ars-to-state.json",'r',encoding='utf-8') as response:
+    ars_to_state = json.load(response)
+    
+def add_geoinfo_to_vacancies(vacancies:pd.DataFrame):
+    # Get Bundesland name fromm ARS
+    vacancies['Bundesland'] = (vacancies['Amtlicher Regionalschlüssel']
+                               .str[:2]
+                               .apply(lambda x: ars_to_state[x]))
+    # Get geo id from Bundesland name
+    vacancies['id'] = vacancies['Bundesland'].apply(lambda x: state_to_id[x])
+
+    return vacancies
+
+def plot_map(vacancies):
+
+    fig = px.choropleth_map(data_frame=vacancies,
+                        geojson=states,
+                        locations='id',
+                        color='Plätze',
+                        color_continuous_scale="Viridis",
+                        # range_color=(0, 12),
+                        # map_style="carto-positron",
+                        map_style='white-bg',
+                        zoom=4.8,
+                        center = {"lat": 51.2, "lon": 9.9167},
+                        opacity=0.5,
+                        hover_name='Bundesland',
+                        hover_data={'id':False,
+                                    'Plätze':True},
+                        )
+    
+    fig.update_layout(title='Freie Stellen (abs.) auf <b>Ausbildung.de</b> (pro Bundesland)',
+                  margin={"r":0,"t":60,"l":0,"b":0},
+                  width=800,
+                  height=600,
+                  )
+
+    return fig
+
+# 0e1117
